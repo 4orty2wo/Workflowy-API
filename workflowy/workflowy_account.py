8aecc555b1ed553ecade45773f03679a1a5a74a9
4orty2wo <sethsantos789@gmail.com>
2023-11-15 17:45:09 -0500

    Trims methods

--- workflowy/workflowy_account.py
+++ workflowy/workflowy_account.py
@@ -6,27 +6,8 @@ class WorkFlowyAccount:
     def __init__(self, session_id):
         self.transport = WorkFlowyTransport(session_id)
         init_data = self.transport.api_request('get_initialization_data', {})
-
         self.user = init_data['user']
-
-        self.username = init_data['settings']['username'] if init_data['settings']['username'] else ''
-        self.email = init_data['settings']['email'] if init_data['settings']['email'] else ''
-        self.itemsCreated = init_data['user']['itemsCreated'] if init_data['user']['itemsCreated'] else ''
-        self.monthlyItemQuota = init_data['user']['monthlyItemQuota'] if init_data['user']['monthlyItemQuota'] else ''
-        self.dateJoined = init_data['user']['dateJoined'] if init_data['user']['dateJoined'] else ''
             
 
     def get_user_info(self):
         return self.user
-
-
-    def get_info(self):
-        return {
-            'username': self.username,
-            'email': self.email,
-            'itemsCreated': self.itemsCreated,
-            'monthlyItemQuota': self.monthlyItemQuota,
-            'dateJoined': self.dateJoined
-        }
-
-    # Add additional methods for other account-related functionalities as needed.
