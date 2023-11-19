d5cce82d675f233323df3709b71f54ba42632e48
4orty2wo <sethsantos789@gmail.com>
2023-11-19 15:24:00 -0500

    Implements account object and getter methods

--- workflowy/workflowy_account.py
+++ workflowy/workflowy_account.py
@@ -6,8 +6,33 @@ class WorkFlowyAccount:
     def __init__(self, session_id):
         self.transport = WorkFlowyTransport(session_id)
         init_data = self.transport.get_initialization_data()
-        self.user = init_data['user']
-            
+        self.email = init_data['user']
+        self.name = init_data['fullName']
+        self.registration_date = init_data['dateJoined']
+        self.monthly_item_quota = init_data['monthlyItemQuota']
+        self.items_created_this_month = init_data['itemsCreated']
+        self.invite_link = init_data['inviteLink']
 
-    def get_user_info(self):
-        return self.user
+
+    def get_email(self):
+        return self.email
+    
+
+    def get_name(self):
+        return self.name
+    
+
+    def get_registration_date(self):
+        return self.registration_date
+    
+
+    def get_monthly_item_quota(self):
+        return self.monthly_item_quota
+    
+
+    def get_items_created_this_month(self):
+        return self.items_created_this_month
+    
+
+    def get_invite_link(self):
+        return self.invite_link
